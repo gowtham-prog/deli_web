64a95f6fb544e08ebc0bcafa29a3cf77f6ca5ae8
Gowtham Kumar Vulluri <gowthamkumarvulluri@gmail.com>
2023-06-06 12:05:47 +0530

    Ready to get deplyed in versel

--- commerce/settings.py
+++ commerce/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = '6ps8j!crjgrxt34cqbqn7x&b3y%(fny8k8nh21+qa)%ws3fh!q'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
-
+ 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
